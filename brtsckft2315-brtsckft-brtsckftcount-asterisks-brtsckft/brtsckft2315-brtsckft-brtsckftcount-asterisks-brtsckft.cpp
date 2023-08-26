cfd9b67ce82eaed814021e89f90905b98f1e07fb
Ajoy Sarker <50762229+AJOYSR@users.noreply.github.com>
2023-08-26 19:08:27 +0600

    Time: 0 ms (100.00%), Space: 6.4 MB (73.10%) - LeetHub

--- brtsckft2315-brtsckft-brtsckftcount-asterisks-brtsckft/brtsckft2315-brtsckft-brtsckftcount-asterisks-brtsckft.cpp
+++ brtsckft2315-brtsckft-brtsckftcount-asterisks-brtsckft/brtsckft2315-brtsckft-brtsckftcount-asterisks-brtsckft.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     int countAsterisks(string s) {
-        int bar=0,count=0;
-        int ans = 0;
-        for(int i=0;i<s.size();i++) {
-            if(s[i]=='*') count ++;
-            if(bar==0 && s[i]=='|') {
-                ans+=count;
-                count = 0;
-                bar++;
-                continue;
-            }
-            if(s[i]=='|') {
-                bar++;
-                if(bar%2) {
-                    ans+=count;
-                    count=0;
-                }
-                else{
-                    count=0;
-                }
-}
-
-     }
-            
-      
-        return ans + count;
+    int n=s.length();
+    if(n==0) return 0;
+    int cnt=0;
+    int as=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='|'){
+             as++;
+        }
+        if(as%2==0 && s[i]=='*'){
+            cnt++;
+                                        
+        }
     }
+    return cnt;
+}
+    
 };
