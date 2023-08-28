fb966282a432461e51a9d29c725a94001c106edd
Ajoy Sarker <50762229+AJOYSR@users.noreply.github.com>
2023-08-28 19:46:31 +0600

    Time: 13 ms (45.26%), Space: 21.3 MB (44.48%) - LeetHub

--- brtsckft2744-brtsckft-brtsckftfind-maximum-number-of-string-pairs-brtsckft/brtsckft2744-brtsckft-brtsckftfind-maximum-number-of-string-pairs-brtsckft.cpp
+++ brtsckft2744-brtsckft-brtsckftfind-maximum-number-of-string-pairs-brtsckft/brtsckft2744-brtsckft-brtsckftfind-maximum-number-of-string-pairs-brtsckft.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int maximumNumberOfStringPairs(vector<string>& words) {
-        int ans;
-        for(int i=0;i<words.size();i++) {
-            for(int j=i+1;j<words.size();j++) {
-                string x = words[i],y = words[j];
-                if((x[0]==y[1] && x[1]==y[0])) ans++;
-         }
-      }
-        return ans;
+        unordered_set<string>mp;
+        int res=0;
+        for(auto it:words){
+            string s=it;
+            reverse(it.begin(),it.end());
+            if(mp.find(it)==mp.end()){
+                mp.insert(s);
+            }
+            else res++;
+           
+        }
+        return res;
     }
 };
