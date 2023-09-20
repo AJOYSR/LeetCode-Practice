d24ab917328b67f264d60ce9dc8641f1a3126bed
Ajoy Sarker <50762229+AJOYSR@users.noreply.github.com>
2023-09-20 12:09:46 +0600

    Time: 38 ms (5.01%), Space: 12.8 MB (5.28%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    // string removeTrailingZeros(string num) {
+    //     string ans;
+    //     int k ;
+    //     for(int i=num.size()-1;i>=0;i--) {
+    //             if(num[i]!='0') {
+    //                 k = i;
+    //                 break;
+    //             }
+    //     }
+    //     ans = num.substr(0,k+1);
+    //     return ans;
+    // }
     string removeTrailingZeros(string num) {
-        string ans;
-        int k ;
-        for(int i=num.size()-1;i>=0;i--) {
-                if(num[i]!='0') {
-                    k = i;
-                    break;
-                }
-        }
-        ans = num.substr(0,k+1);
-        return ans;
+        return regex_replace(num, regex("0+$"), "");
     }
 };
