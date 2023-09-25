f5e38b758e311a788d6b5909c34d700ea7f6b7ed
Ajoy Sarker <50762229+AJOYSR@users.noreply.github.com>
2023-09-25 18:51:38 +0600

    Time: 15 ms (96.81%), Space: 25.9 MB (10.78%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+             int n = nums.size();
+            // Make a copy of nums
+            vector<int> numsCopy(n);
+            for (int i = 0; i < n; i++)
+            {
+                numsCopy[i] = nums[i];
+            }
+            
+            // Rotate the elements.
+            for (int i = 0; i < n; i++)
+            {
+                nums[(i + k)%n] = numsCopy[i];
+            }
+        
+    }
+};
