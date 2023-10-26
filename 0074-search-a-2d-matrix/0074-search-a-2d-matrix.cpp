a43e1ab8eb83b245ce722090e5a04a11701839d9
Ajoy Sarker <50762229+AJOYSR@users.noreply.github.com>
2023-10-26 12:08:36 +0600

    Time: 3 ms (70.32%), Space: 9.7 MB (58.19%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,16 +3,20 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix[0].size();
         int n = matrix.size();
-        int i = 0, j = m-1;
-        while( i < n and j>=0 ) {
+        
+        int low = 0, high = (n*m)-1;
+        
+        while( low<=high ) {
+            int mid = (high-low)/2 +  low;
+            int i = (mid / m), j = mid % m;
             if(matrix[i][j] == target) {
                 return true;
             }
             
-            if(matrix[i][j]>target) {
-                j--;
+            if(matrix[i][j]<target) {
+                low = mid + 1;
             } else {
-                i++;
+                high = mid - 1;
             }
         }
         return false;
