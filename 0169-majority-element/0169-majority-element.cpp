7a5626634f2503b85323684a9e15e398391ee8b5
Ajoy Sarker <50762229+AJOYSR@users.noreply.github.com>
2023-10-28 19:22:59 +0600

    Time: 7 ms (97.55%), Space: 19.9 MB (41.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,6 +4,8 @@ public:
         map<int,int>mp;
         int ans;
         int n = nums.size();
+        // sort(nums.begin(),nums.end());
+        
         for(int i=0;i<nums.size();i++)
         {
             mp[nums[i]]++;
