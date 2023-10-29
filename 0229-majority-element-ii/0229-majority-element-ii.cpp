dcbd9609d8cd625027b68bbd6b41bff9296c7735
Ajoy Sarker <50762229+AJOYSR@users.noreply.github.com>
2023-10-29 11:29:02 +0600

    Time: 7 ms (87.22%), Space: 16.2 MB (62.47%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,21 +2,34 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
        
-        map<int,int>mp;
-        vector<int> ans;
-        set<int> st;
         int n = nums.size();
         // sort(nums.begin(),nums.end());
-        
+        int c1 = 0, c2 = 0 , num1 = -1, num2 = -1;
         for(int i=0;i<nums.size();i++)
         {
-            mp[nums[i]]++;
-            if(mp[nums[i]]>n/3){
-                st.insert(nums[i]);
-               
-            }
+           if(nums[i]==num1) c1++;
+           else if(nums[i]==num2) c2++;
+           else if(c1 == 0) {
+               num1 = nums[i];
+               c1 += 1;
+           }
+           else if(c2 == 0) {
+               num2 = nums[i];
+               c2 += 1;
+           }
+           else {
+               c1--;
+               c2 --;
+           }
         }
-        for( int elm : st)  ans.push_back(elm);
+        vector<int> ans;
+        c1 = c2 = 0;
+        for(int i = 0; i < n ; i++) {
+            if(nums[i] == num1) c1 ++;
+            else if (nums[i] == num2) c2++;
+        }
+        if(c1 > n/3) ans.push_back(num1);
+        if(c2 > n/3) ans.push_back(num2);
         return ans;
     }
 };
