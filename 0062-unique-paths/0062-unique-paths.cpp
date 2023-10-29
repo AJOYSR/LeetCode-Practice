58ffba48719694ec8c84f44e07ea057d06b9d01d
Ajoy Sarker <50762229+AJOYSR@users.noreply.github.com>
2023-10-29 19:45:49 +0600

    Time: 0 ms (100.00%), Space: 6.8 MB (11.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,20 @@
 class Solution {
+
+private:
+    int total_paths(int row, int col, vector<vector<int>> &paths, int m, int n){
+        if(row>=m || col>=n) return 0;
+        if(row == m-1 && col == n-1) return 1;
+
+        if(paths[row][col] != -1) return paths[row][col];
+
+        return paths[row][col] = total_paths(row, col+1, paths, m, n) + total_paths(row+1, col, paths, m, n);
+    }
+
 public:
     int uniquePaths(int m, int n) {
-        vector<int> vec(n,1);
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
-                vec[j]+= vec[j-1];
-                
-            }
-        }
-        return vec[n-1];
+        vector<vector<int>> paths(m, vector<int> (n, -1));
+        int row = 0, col = 0;
+
+        return total_paths(row, col, paths, m, n);
     }
 };
